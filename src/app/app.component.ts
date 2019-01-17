63fecd54a25be770bd1de80b40d94bb5191f3aea
spikedios <d.ivan@hotmail.com>
2019-01-17 15:40:08 +0100

    comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,35 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gig';
+	title = 'gig';
 
 
 
-countries = [
-    {"key":"1","value":"1"},
-    {"key":"1","value":"2"},
-    {"key":"1","value":"3"},
-    {"key":"1","value":"4"},
-    {"key":"1","value":"5"}
-];
+	countries = [
+	{"key":"1","value":"1"},
+	{"key":"1","value":"2"},
+	{"key":"1","value":"3"},
+	{"key":"1","value":"4"},
+	{"key":"1","value":"5"}
+	];
 
-countrycode = [
-    {"key":"1","value":"+1"},
-    {"key":"2","value":"+2"},
-    {"key":"3","value":"+3"},
-    {"key":"4","value":"+4"},
-    {"key":"5","value":"+5"}
-];
+	countrycode = [
+	{"key":"1","value":"+1"},
+	{"key":"2","value":"+2"},
+	{"key":"3","value":"+3"},
+	{"key":"4","value":"+4"},
+	{"key":"5","value":"+5"}
+	];
 
-
-
-
-styleGuide = {
-    caretClass: 'caret',
-    selectBoxClass: 'dropdown-wrapper',
-    selectMenuClass: 'dropdown',
-    optionsClass: 'option' 
-};
+	styleGuide = {
+		caretClass: 'caret',
+		selectBoxClass: 'dropdown-wrapper',
+		selectMenuClass: 'dropdown',
+		optionsClass: 'option' 
+	};
 }
